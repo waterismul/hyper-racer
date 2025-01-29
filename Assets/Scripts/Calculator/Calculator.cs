f2fbe6fc9f000960517d4fee88f253b063a3644a
yu <yuuuuufooo@gmail.com>
2025-01-29 19:27:48 +0900

    테스트환경

---
+++ Assets/Scripts/Calculator/Calculator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Calculator 
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Sub(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Mul(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Div(int a, int b)
+    {
+        return a / b;
+    }
+}
--- Assets/Scripts/Game/RoadController.cs
+++ Assets/Scripts/Game/RoadController.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using Unity;
+//using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
